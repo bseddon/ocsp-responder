753f19e347580980130118a2c3a81dcafd138410
bseddon <bill.seddon@lyquidity.com>
2021-09-26 21:14:17 +0000

    Fix for Linux

--- src/autoload.php
+++ src/autoload.php
@@ -4,8 +4,9 @@ function responder_autoload( $classname )
 {
 	$prefix = 'PKIX\\';
 	if ( strpos( $classname, $prefix ) !== 0 ) return false;
-	$filename = substr( $classname, strlen( $prefix ) ) . '.php';
-	$filename = str_replace('\\', '/', $filename);
+	// $filename = substr( $classname, strlen( $prefix ) ) . '.php';
+	// $filename = str_replace('\\', '/', $filename);
+	$filename = str_replace('\\', '/', $classname . '.php' );
 	if ( ! file_exists(  __DIR__ . "/$filename" ) ) return false;
 	// $filename = "$classname.php";
 	require_once  __DIR__ . "/$filename";
